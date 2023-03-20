5fb69b33a0dc84b97407eeae9f4dd9c1d7695175
Michael-Robert1 <mrobert@hartford.edu>
2023-03-20 17:02:54 -0400

    Update RunMatrix.java
    Add method names

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+
+    Matrix.initiateMatrix();
+    Matrix.populateMatrix();
+    Matrix.printMatrix();
+    Matrix.flipMatrix();
+    Matrix.printMatrix();
+
   }
 }
