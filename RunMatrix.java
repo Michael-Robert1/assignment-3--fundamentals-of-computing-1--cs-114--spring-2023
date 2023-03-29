a22f2ba94bd501b362fe0b80136647dd64313890
Michael-Robert1 <mrobert@hartford.edu>
2023-03-29 16:37:08 -0400

    Corrections to naming

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    matrix1 = new Matrix();
 
-    Matrix.initiateMatrix();
-    Matrix.populateMatrix();
-    Matrix.printMatrix();
-    Matrix.flipMatrix();
-    Matrix.printMatrix();
+    matrix1.initiateMatrix(1);
+    matrix1.populateMatrix();
+    matrix1.printMatrix();
+    matrix1.flipMatrix();
+    matrix1.printMatrix();
 
   }
 }
