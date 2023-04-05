5dbb30bc0ee40f2b59faf2c3e0409e33af66f3bd
Michael-Robert1 <mrobert@hartford.edu>
2023-04-04 23:56:09 -0400

    Update RunMatrix.java

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,23 +1,48 @@
+// *****************************************************************************
+// RunMatrix.java       Author: Michael Robert
+//
+// Submission for Assignment 3 in CS 114
+// *****************************************************************************
+
+//Import scanner library
 import java.util.Scanner;
 
 public class RunMatrix {
+  // ---------------------------------------------------------------------------
+  //  Construct, populate, and flip a matrix based on user-input
+  // ---------------------------------------------------------------------------
   public static void main(String[] args) {
 
-    Matrix matrix = new Matrix();
-
+    //Construct a new scanner called input
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Please enter the desired width of the matrix")
+    //Prompt the user for the size of the matrix
+    System.out.print("\n\n\tPlease enter the desired size of the matrix: ");
 
-    int matrixWidth = input.nextInt();
+    //Store the user-entered integer in an integer named matrixWidth
+    int matrixSize = input.nextInt();
 
-    matrix.initiateMatrix(matrixWidth);
+    //Confirm the matrix dimension
+    System.out.printf("\n\n\tThe size of your matrix is %d x %d\n\n",
+      matrixSize, matrixSize);
 
-    matrix.populateMatrix();
-
+    //Construct and print matrix with a user-specified size
+    System.out.println(
+      "\n\tPrinting your newly constructed, zero filled matrix...");
+    Matrix matrix = new Matrix(matrixSize);
     matrix.printMatrix();
+
+    //Populate and re-print matrix
+    System.out.println("\n\tPopulating and re-printing your matrix...");
+    matrix.populateMatrix();
+    matrix.printMatrix();
+
+    //Flip and re-print matrix
+    System.out.println("\n\tFlipping and re-printing your matrix...");
     matrix.flipMatrix();
     matrix.printMatrix();
 
+    //Closes the scanner to prevent a resource leak
+    input.close();
   }
 }
